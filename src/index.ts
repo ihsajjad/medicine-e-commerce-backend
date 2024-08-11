e96f8f5cf210a4b8cf168488765f825c8d383715
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-08-11 20:29:28 +0600

    Initial setup done

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+/**
+ * Title: Node-Express Project Index File
+ * Description: This file serves as the entry point for the Node.js Express application. It initializes the server and sets up routing and middleware.
+ * Author: MD Iftekher Hossen Sajjad
+ * Date: 11/8/2024
+ */
+
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+// connect mongodb database
+mongoose
+  .connect(process.env.MONGODB_URI as string, { dbName: "care-cube" })
+  .then(() => console.log("DB is connected"));
+
+// routes
+
+// swagger docs function
+// swaggerDocs(app, parseInt(port.toString()));
+
+app.get("/", (_, res) => {
+  res.json({ message: "Server is running on port : 3000" });
+});
+
+app.listen(port, () =>
+  console.log(`Server is running on port : http://localhost:${port}`)
+);
